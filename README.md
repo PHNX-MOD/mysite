509528e5c650972bc9a7f31dbc9e59db3fa19ee3
PHNX-MOD <modith.hadya@gmail.com>
2020-04-25 13:25:11 +0000

    Commit message

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# **Orion online Store**
-## Orion store is a online shopping application
+# **Orion Cart**
+## Orion Cart is a online shopping application
  The application is built with Django framework which includes the following functionalities
 - user login
 - 
@@ -11,3 +11,6 @@ Some Git commands
 `git add *`
 `git commit -m "Commit message"`
 `git push origin master`
+
+
+python -m pip install Pillow
--- store/views.py
+++ store/views.py
@@ -4,11 +4,24 @@ from .models import Product
 from math import ceil
 
 def home(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    #products = Product.objects.all()
+    #print(products)
+    #n = len(products)
+    #nSlides = n//4 + ceil((n/4)-(n//4))
+    
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+      prod = Product.objects.filter(category = cat)
+      n = len(prod)
+      nSlides = n//4 + ceil((n/4)-(n//4))
+      allProds.append([prod,range(1, nSlides), nSlides])
+
+    #params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    #allProds = [[products, range(1, nSlides), nSlides],[products, range(1, nSlides), nSlides]]
+    
+    params = {'allProds': allProds}
     return render(request, 'store/home.html', params)
 
 def about(request):
