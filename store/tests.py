49c3d30572fa2e31471935bc14e2129b3f5265c1
Aécio Costa <aeciovc@gmail.com>
2020-04-26 09:55:33 +0300

    Adding tests for static pages

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PageTests(TestCase):
+
+    def test_about_page(self):
+        response = self.client.get("/about/")
+        self.assertContains(response, "this is the about page")
