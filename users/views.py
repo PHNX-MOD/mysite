607a97f9bbc9ede4e7fc3e0a7c68476ea8a4a717
Modith hadya leelananda <modith.hadya@gmail.com>
2020-04-26 11:18:46 +0300

    user creation updated

--- users/views.py
+++ users/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterationForm
 
 def register(request):
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = UserRegisterationForm(request.POST)
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
       messages.success(request, f'Account created for {username}!')
       return redirect('home')
   else:
-    form = UserCreationForm()
+    form = UserRegisterationForm()
   return render(request, 'users/register.html', {'form':form})
